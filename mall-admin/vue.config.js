0ed0f4198263ccbf268d22422a0d13c722dc245a
leoxiaoge <1269286430@qq.com>
2021-04-12 12:22:40 +0800

    commit

--- mall-admin/vue.config.js
+++ mall-admin/vue.config.js
@@ -29,7 +29,17 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
-    port: port
+    port: port,
+    proxy: {
+      '/': {
+        target: 'http://116.62.158.111:8080', //API服务器的地址
+        ws: true,  //代理websockets
+        changeOrigin: true, // 虚拟的站点需要更管origin
+        pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
+          '^/': ''
+        }
+      }
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
--- mall-wx/config/api.js
+++ mall-wx/config/api.js
@@ -1,10 +1,10 @@
 // 以下是业务服务器API地址
 // 本机开发时使用
-var WxApiRoot = 'http://localhost:8080/wx/';
+// var WxApiRoot = 'http://localhost:8080/wx/';
 // 局域网测试使用
 // var WxApiRoot = 'http://192.168.1.3:8080/wx/';
 // 云平台部署时使用
-// var WxApiRoot = 'http://122.51.199.160:8080/wx/';
+var WxApiRoot = 'http://116.62.158.111:8080/wx/';
 // 云平台上线时使用
 // var WxApiRoot = 'https://www.menethil.com.cn/wx/';
 
